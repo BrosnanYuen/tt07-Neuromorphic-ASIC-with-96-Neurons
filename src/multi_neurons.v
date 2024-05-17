d326a5fdb12e5f40e896a58e9c0018e17f297dd8
BrosnanYuen <130252102+BrosnanYuen@users.noreply.github.com>
2024-05-16 18:08:24 -0700

    Update multi_neurons.v

--- src/multi_neurons.v
+++ src/multi_neurons.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_multi_neurons_brosnanyuen #(parameter NEURONS = 24, INPUTS=8, OUTPUTS=8, MEMORY=8) (
+module tt_um_multi_neurons_brosnanyuen #(parameter NEURONS = 96, INPUTS=8, OUTPUTS=8, MEMORY=8) (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
